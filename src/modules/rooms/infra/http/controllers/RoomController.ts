5d0d50f8425899fb060412ae183e7c25f3eb0b09
Lucas <lucas.raziel@gmail.com>
2020-06-27 21:42:08 -0300

    room show lamps state routes

--- src/modules/rooms/infra/http/controllers/RoomController.ts
+++ src/modules/rooms/infra/http/controllers/RoomController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 import ListRoomService from '@modules/rooms/services/ListRoomsService';
+import GetStateOfLampService from '@modules/rooms/services/GetStateOfLampsService';
 
 export default class RoomController {
   public async index(request: Request, response: Response): Promise<Response> {
@@ -9,5 +10,15 @@ export default class RoomController {
     const rooms = await listRoomService.execute();
 
     return response.json(rooms);
+  }
+
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { name } = request.params;
+
+    const getStateOfLampSarvice = container.resolve(GetStateOfLampService);
+
+    const lamps = await getStateOfLampSarvice.execute(name);
+
+    return response.json(lamps);
   }
 }
--- src/modules/rooms/infra/http/routes/RoomsRouter.ts
+++ src/modules/rooms/infra/http/routes/RoomsRouter.ts
@@ -1,10 +1,22 @@
 import { Router } from 'express';
 
+import { celebrate, Segments, Joi } from 'celebrate';
+
 import RoomController from '@modules/rooms/infra/http/controllers/RoomController';
 
 const roomsRouter = Router();
 
 const roomController = new RoomController();
+
+roomsRouter.get(
+  '/:name',
+  celebrate({
+    [Segments.PARAMS]: {
+      name: Joi.string().required(),
+    },
+  }),
+  roomController.show
+);
 
 roomsRouter.get('/', roomController.index);
 
